c0ba93cac12f9c225f94f88d58b5741e6f55317b
cynynpri <pkmn_rgds@yahoo.co.jp>
2022-08-21 23:38:53 +0900

    improved launch process.

--- launch.sh
+++ launch.sh
@@ -4,6 +4,6 @@ export SHELL=/bin/bash
 
 /bin/bash -c "PATH=${PATH}:/home/pyenv/.pyenv/versions/3.9.12/bin"
 
-/home/pyenv/.pyenv/versions/3.9.12/bin/python -m pip freeze > ./requirements.txt
+#/home/pyenv/.pyenv/versions/3.9.12/bin/python -m pip freeze > ./requirements.txt
 
 /home/pyenv/.pyenv/versions/3.9.12/bin/jupyter-lab --port 8888 --ip=0.0.0.0
